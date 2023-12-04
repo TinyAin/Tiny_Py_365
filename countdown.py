4af7b1d2736ac4ecc0e2d7b254e83c4a8eebe0cc
TinyAin <135683644+TinyAin@users.noreply.github.com>
2023-12-04 13:34:05 +0800

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,13 +1,13 @@
 import time
 
-def countdown(t):
-    while t > 0:
-        mins, secs = divmod(t, 60)
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(1)
-        t -= 1
-    print("倒计时结束")
+        time_sec -= 1
 
-t = int(input("请输入倒计时的秒数："))
-countdown(t)
+    print("stop")
+
+countdown(5)
